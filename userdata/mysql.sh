b48c1848cb1b0c81b6958c848492c7ad16a7ba78
Ayobami <129845010+yobami12@users.noreply.github.com>
2024-09-13 23:04:35 +0200

    Update mysql.sh

--- userdata/mysql.sh
+++ userdata/mysql.sh
@@ -18,6 +18,6 @@ sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' O
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'ayobami'@'localhost' identified by 'host123'"
-sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'ayobami'@'%' identified by 'admin123'"
-sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'ayobami'@'%' identified by 'host123'"
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/Aprofile-project/src/main/resources/db_backup.sql
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
