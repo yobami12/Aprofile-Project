f37aa735f7638dd2d453fb1d41ce47bf9ba1935e
ayobami <agbedaviesayobami@yahoo.com>
2024-09-17 14:43:33 +0200

    aws-cicd

---
+++ vagrant/Automated_provisioning_WinMacIntel/memcache.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo yum update -y
+#sudo yum install epel-release -y
+#sudo dnf config-manager --disable epel
+sudo dnf config-manager --disable extras
+sudo dnf clean all
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+sudo dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
+sudo crb install
+sudo /usr/bin/crb enable
+sudo dnf config-manager --set-enable remi
+sudo dnf update -y
+sudo dnf install memcached -y
+sudo systemctl start memcached
+sudo systemctl enable memcached
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
+sudo systemctl restart memcached
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+firewall-cmd --add-port=11211/tcp
+firewall-cmd --runtime-to-permanent
+firewall-cmd --add-port=11111/udp
+firewall-cmd --runtime-to-permanent
+sudo memcached -p 11211 -U 11111 -u memcached -d
---
+++ vagrant/Automated_provisioning_WinMacIntel/mysql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+DATABASE_PASS='host123'
+sudo yum update -y
+#sudo yum install epel-release -y
+#sudo dnf config-manager --disable epel
+sudo dnf config-manager --disable extras
+sudo dnf clean all
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+sudo dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
+sudo crb install
+sudo /usr/bin/crb enable
+sudo dnf config-manager --set-enable remi
+sudo dnf update -y
+sudo yum install git zip unzip -y
+sudo yum install mariadb-server -y
+
+
+echo "# starting & enabling mariadb-server #"
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+cd /tmp/
+git clone -b main https://github.com/yobami12/Aprofile-Project.git
+#restore the dump file for the application
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'ayobami'@'localhost' identified by 'host123'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'ayobami'@'%' identified by 'host123'"
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/Aprofile-Project/src/main/resources/db_backup.sql
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+# Restart mariadb-server
+sudo systemctl restart mariadb
+
+
+#starting the firewall and allowing the mariadb to access from port no. 3306
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart mariadb
