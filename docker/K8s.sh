f18037e212a0153de146dcc025a26939c6837600
Ayobami <129845010+yobami12@users.noreply.github.com>
2025-04-27 20:50:29 +0200

    Update K8s.sh

--- docker/K8s.sh
+++ docker/K8s.sh
@@ -121,6 +121,7 @@ sed -i '/ swap / s/^/#/' /etc/fstab
 #sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 #sudo chown $(id -u):$(id -g) $HOME/.kube/config
 #sed -i 's/cgroupDriver.*/cgroupDriver: systemd/g' /var/lib/kubelet/config.yaml
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 ###Verify the Cluster Status:
 
