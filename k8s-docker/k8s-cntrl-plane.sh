67843a7580dec269c31bc7d77b962b8b1668edd8
Ayobami <129845010+yobami12@users.noreply.github.com>
2025-04-29 23:22:15 +0200

    Update k8s-cntrl-plane.sh

--- k8s-docker/k8s-cntrl-plane.sh
+++ k8s-docker/k8s-cntrl-plane.sh
@@ -174,6 +174,11 @@ echo "*****************************************************"
 echo "*********INSTALLING INGRESS CONTROLLER***************"
 echo "*****************************************************"
 echo " "
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/cloud/deploy.yaml
 
 echo " "
