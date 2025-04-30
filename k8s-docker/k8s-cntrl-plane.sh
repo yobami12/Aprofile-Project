3d2a2938f7451c433a1d853763dc3517e88b54a4
Ayobami <129845010+yobami12@users.noreply.github.com>
2025-04-30 10:18:55 +0200

    Update k8s-cntrl-plane.sh

--- k8s-docker/k8s-cntrl-plane.sh
+++ k8s-docker/k8s-cntrl-plane.sh
@@ -179,7 +179,8 @@ sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/cloud/deploy.yaml
+helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/cloud/deploy.yaml
 
 echo " "
 echo "*******************************************"
