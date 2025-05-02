cfb3bba18c26dee17608c686fc2f1996bb099cbb
Ayobami <129845010+yobami12@users.noreply.github.com>
2025-05-02 18:17:44 +0200

    Update k8s-cntrl-plane.sh

--- k8s-docker/k8s-cntrl-plane.sh
+++ k8s-docker/k8s-cntrl-plane.sh
@@ -34,8 +34,10 @@ echo " "
 echo "##########INSTALLING KUBEAMD, KUBLET AND KUBECTL###########"
 echo " "
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+curl -LO https://dl.k8s.io/release/v1.33.0/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v1.32.0/bin/linux/amd64/kubectl.sha256
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
